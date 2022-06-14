58d4ae72f51949707e6c20e8621d0e7e576ae3d6
Rafheros <rafaelheros80@gmail.com>
2022-06-14 00:21:36 -0300

    remove ts

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-root.render(<App />);
