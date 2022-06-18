7aa8c8f13409b2613c0133b39d8fd70417856cd6
Rafheros <rafaelheros80@gmail.com>
2022-06-18 03:23:44 -0300

    correction to angle threshold logic

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
 
   const bind = usePinch(({ offset: [scale, angle] }) => { 
     setScale([scale,scale,scale]);
-    let angleThreshold = Math.abs((newAngle - angle)/newAngle);
+    let angleThreshold = Math.abs((newAngle[1] - angle)/newAngle);
     if(angleThreshold > 0.1) { 
       setNewAngle([-90,angle*0.2,0]);
     }
