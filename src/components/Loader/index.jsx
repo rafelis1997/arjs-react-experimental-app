9f00dc1c8c6db9117ea20f4a4b6d22ec79ebebc1
Rafheros <rafaelheros80@gmail.com>
2022-06-17 21:03:13 -0300

    Loader fix

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,20 +1,22 @@
 import "./style-loader.css";
 
-import { useProgress } from "@react-three/drei";
+import { Html, useProgress } from "@react-three/drei";
 
 export default function Loader() {
   const { progress } = useProgress();
   return (
-    <div className="container-loader">
-      <div className="loadScreen">
-        <h1>Loading Experience</h1>
+    <Html center>
+      <div className="container-loader">
+        <div className="loadScreen">
+          <h1>Loading Experience</h1>
 
-        <div>
-          <div></div>
-          <div>{progress} % loaded</div>
-          <div></div>
+          <div>
+            <div></div>
+            <div>{progress} % loaded</div>
+            <div></div>
+          </div>
         </div>
       </div>
-    </div>
+    </Html>
   );
 }
